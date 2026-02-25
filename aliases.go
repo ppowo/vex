a02747aa129e287a958ed9489a1cec6085f9659e
punowo <223329946+ppowo@users.noreply.github.com>
2026-02-25 23:38:16 +0100

    remove zai

--- aliases.go
+++ aliases.go
@@ -4,6 +4,5 @@ package main
 // Add new entries here and rebuild.
 var aliases = map[string]string{
 	"openrouter": "OPENROUTER_API_KEY",
-	"zai":        "ZAI_API_KEY",
-	"opencodezen":   "OPENCODE_API_KEY",
+	"opencodezen": "OPENCODE_API_KEY",
 }
